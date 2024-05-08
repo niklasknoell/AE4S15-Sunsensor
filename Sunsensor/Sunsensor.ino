ad7ef16f1dd815e36fd244374ae8a07f716305b3
Leo <l.b.besse@student.tudelft.nl>
2024-05-08 12:19:25 +0200

    Added python visualiser

--- Sunsensor/Sunsensor.ino
+++ Sunsensor/Sunsensor.ino
@@ -5,34 +5,49 @@
 // should be first 2 +x, then 2 +y, then 2 -x, then 2-y
 int sens_array[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
+int too_dark_threshold = 1015;
+int too_bright_threshold = 3;
+
+void get_sun_sensor(int *ADC_readings, BLA::Matrix<3> *s);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // just to spit out some status
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  int readings[8];
-
-  for (int i = 0; i < 8; i++) {
-    readings[i] = analogRead(sens_array[i]);
-  }
-
-  int error_threshold = 100;
-
-
-  int intensities[4];
+void get_sun_sensor(int *ADC_readings, BLA::Matrix<3>* s){
+  
+  int intensities[4] = {0};
 
   for (int i = 0; i<4; i++) {
-    // basically, if the two ldrs on each side disagree by too much we need to do something maybe
-    if (abs(readings[i*2] - readings[i*2 + 1]) > error_threshold) { 
-      // set error or choose which one to use in some intelligent way
+    // basically, if one LDR is outside the thresholds, use the other one
+    // if they are both outside the thresholds, return zero sun vector
+    if ((ADC_readings[i*2] > too_dark_threshold) || (ADC_readings[i*2] < too_bright_threshold)) 
+    { 
+      if ((ADC_readings[i*2+1] > too_dark_threshold) || (ADC_readings[i*2+1] < too_bright_threshold))
+      {
+        *s = {0, 0, 0};
+        return 1; // Both sensors are outside expected range, return 1
+      }
+      else
+      {
+        intensities[i] = ADC_readings[i*2+1]; // Only use the second LDR
+      }
     }
-    else {
-      intensities[i] = (readings[i*2] + readings[i*2 + 1])/2; // average of ldrs
+    else if((ADC_readings[i*2+1] > too_dark_threshold) || (ADC_readings[i*2+1] < too_bright_threshold))
+    {
+      intensities[i] = ADC_readings[i*2];
     }
+    else 
+    {
+      intensities[i] = (ADC_readings[i*2] + ADC_readings[i*2 + 1])/2; // average of ldrs
+    }
+  }
+  
+  // Make the values negative s.t. the signal scales with brightness
+  for (int i = 0; i < 4; i++)
+  {
+    intensities[i] = -intensities[i];
   }
 
   // now we have figured out the intensities from input data
@@ -44,20 +59,43 @@ void loop() {
 
   // maybe reduce rows by one if we find bad data on one side of pyramid?
 
-  BLA::Matrix<4,3> N = {1,0,1,
-                        1,0,1, // bogus values, gotta put in right angles and stuff
-                        1,0,1,
-                        1,0,1};
+  BLA::Matrix<4,3> N = {0,-2,1,
+                        0,2,1,
+                        2,0,1,
+                        -2,0,1};
 
   BLA::Matrix<4,1> I = {intensities[0], intensities[1], intensities[2], intensities[3]};
 
-  BLA::Matrix<3,1> s = Inverse(~N * N) * ~N * I;
+  *s = Inverse(~N * N) * ~N * I;
 
-  // should be sun vector in local x-y-z frame printed on sensor 
+  return 0;
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  int readings[8];
 
+  for (int i = 0; i < 8; i++) {
+    readings[i] = analogRead(sens_array[i]);
+  }
 
+  BLA::Matrix<3,1> s;
+  get_sun_sensor(readings, &s);
 
+  // Serial.print("Raw values: \n");
+  // for (int i = 0; i < 8; i++){
+  //   Serial.print("Sensor1: ");
+  //   Serial.print(readings[i], DEC);
+  //   Serial.print(" ");
+  // }
 
+  delay(100);
 
+  Serial.print("X:");
+  Serial.print(s(0), DEC);
+  Serial.print(" Y:");
+  Serial.print(s(1), DEC);
+  Serial.print(" Z:");
+  Serial.print(s(2), DEC);
+  Serial.print("\n");
 }
---
+++ Sunsensor/run.py
@@ -0,0 +1,37 @@
+import serial
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+
+running = True
+
+
+def get_coords(bytestring):
+    l = bytestring.split()
+    x = float(l[0][2:])
+    y = float(l[1][2:])
+    z = float(l[2][2:])
+    return [x,y,z]
+
+with serial.Serial('/dev/ttyACM0', timeout=1) as ser:
+
+    ax = plt.figure().add_subplot(projection='3d')
+    x,y,z = [0,0,0] # Location of sun vector on 3d plot
+    plt.ion()
+
+    while running:
+        try:
+            coords = get_coords(ser.readline())
+            print(coords)
+            ax.cla()
+            ax.set_xlabel("X")
+            ax.set_ylabel("Y")
+            ax.set_zlabel("Z")
+            ax.set_title("Sun vector")
+            ax.quiver(x, y, z, *coords, length=0.1, normalize=True)
+            plt.draw()
+            plt.pause(0.001)
+
+        except Exception as e:
+            print(e)
